4eb665d3b90e66533e5f463b178c9f2cdd10deff
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-06 02:21:47 +0300

    Time: 33 ms (16.15%), Space: 47.1 MB (16.75%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,20 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         //longest substring ---> /Dinamic sliding window
-        HashMap<Character, Integer> freq = new HashMap<>();
-        int l = 0, ans = 0, maxFreq = 0;
-        for (int r = 0; r < s.length(); r++) {
-            char rc = s.charAt(r);
-            freq.put(rc, freq.getOrDefault(rc, 0) + 1);
-            maxFreq = Math.max(maxFreq, freq.get(rc));
-            while (r - l + 1 - maxFreq > k) {
-                char lc = s.charAt(l);
-                freq.put(lc, freq.get(lc) - 1);
-                if (freq.get(lc) == 0)
-                    freq.remove(lc);
-                l++;
+        HashMap<Character,Integer> freq = new HashMap<>();
+        int l=0,ans=0,maxFreq=0;
+        for(int r=0;r<s.length();r++){
+            freq.put(s.charAt(r),freq.getOrDefault(s.charAt(r),0)+1);
+            maxFreq=Integer.max(maxFreq,freq.get(s.charAt(r)));
+            while(r-l+1-maxFreq>k){
+                 freq.put(s.charAt(l),freq.getOrDefault(s.charAt(l),0)-1);
+                 if(freq.get(s.charAt(l))==0) freq.remove(s.charAt(l));
+                 l++;
             }
-            ans = Integer.max(r - l + 1, ans);
+            ans = Integer.max(r-l+1,ans);
         }
         return ans;
     }
