a4ee620d80f0804703444510377b03e92179a6e4
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-06 02:09:03 +0300

    Time: 12 ms (51.51%), Space: 46.8 MB (59.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,32 +2,18 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> indxs = new ArrayList<>();
         if (s.length() < p.length()) return indxs;
-
-        String window = "";
-        for (int i = 0; i < p.length(); i++) {
-            window += s.charAt(i);
+        int[] arrP= new int[26];
+        int[] arrS= new int[26];
+        for(int i=0;i<p.length();i++){
+            arrP[p.charAt(i)-'a']++;
+            arrS[s.charAt(i)-'a']++;
         }
-
-        String sortedP = sortString(p);
-        
-        for (int i = p.length(); i < s.length(); i++) {
-            String sortedWindow = sortString(window);
-            if (sortedWindow.equals(sortedP)) {
-                indxs.add(i - p.length());
-            }
-            window = window.substring(1) + s.charAt(i);
+        if(Arrays.equals(arrP,arrS)) indxs.add(0);
+        for(int i=p.length();i<s.length();i++){
+            arrS[s.charAt(i)-'a']++;
+            arrS[s.charAt(i-p.length())-'a']--;
+            if(Arrays.equals(arrP,arrS)) indxs.add(i-p.length()+1);
         }
-        
-        if (sortString(window).equals(sortedP)) {
-            indxs.add(s.length() - p.length());
-        }
-
         return indxs;
-    }
-
-    public String sortString(String s) {
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-        return new String(arr);
     }
 }
