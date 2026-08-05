557767945fc97e954849698210fe003481c75fd5
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-05 08:39:39 +0300

    Time: 11 ms (66.01%), Space: 46.9 MB (59.29%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,21 +1,30 @@
 class Solution {
-    public String sortString(String s) {
-        char[] arrChar = s.toCharArray();//from string to array of characters
-        Arrays.sort(arrChar);
-        String sortedString = new String(arrChar); //CharArr->string
-        return sortedString;
+    public boolean equal(int[] s1, int[] s2) {
+        for (int i = 0; i < 26; i++) {
+            if (s1[i] != s2[i])
+                return false;
+        }
+        return true;
     }
 
     public List<Integer> findAnagrams(String s, String p) {
-
         ArrayList<Integer> indxs = new ArrayList<>();
-        String P = sortString(p);
-        for (int i = 0; i <=s.length() - p.length(); i++) {
-            String w = s.substring(i, i + p.length());
-            String W = sortString(w);
-            if (W.equals(P)) {
-                indxs.add(i);
-            }
+        if (s == null || p == null || s.length() < p.length()) {
+            return indxs;
+        }
+        int[] pCnt = new int[26];
+        int[] sCnt = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            pCnt[p.charAt(i) - 'a']++;
+            sCnt[s.charAt(i) - 'a']++;
+        }
+        if (equal(pCnt, sCnt))
+            indxs.add(0);
+        for (int i = p.length(); i < s.length(); i++) {
+            sCnt[s.charAt(i) - 'a']++;
+            sCnt[s.charAt(i - p.length()) - 'a']--;
+            if (equal(pCnt, sCnt))
+                indxs.add(i - p.length() + 1);
         }
         return indxs;
     }
