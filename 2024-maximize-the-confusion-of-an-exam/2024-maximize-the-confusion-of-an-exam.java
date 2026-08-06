913570f2109041111461d6d110fd82187194abc1
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-06 03:04:27 +0300

    Time: 10 ms (95.94%), Space: 46.1 MB (96.54%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
         //Dinamic Sliding window
-        HashMap<Character,Integer> freq = new HashMap<>();
+        int[] freq = new int[26];
         int l=0,ans=0,maxFreq=0;
         for(int r=0;r<answerKey.length();r++){
             char rc=answerKey.charAt(r);
-            freq.put(rc,freq.getOrDefault(rc,0)+1);//T-4 F-2
-            maxFreq=Integer.max(maxFreq,freq.get(rc));//4
+            freq[rc-'A']++;
+            maxFreq=Integer.max(maxFreq,freq[rc-'A']);
             while(r-l+1-maxFreq>k){
                 char lc =answerKey.charAt(l);
-                freq.put(lc,freq.getOrDefault(lc,0)-1);
+                freq[lc-'A']--;
                 l++;
             }
-            ans=Integer.max(ans,r-l+1);//4
+            ans=Integer.max(ans,r-l+1);
         }
         return ans;
     }
