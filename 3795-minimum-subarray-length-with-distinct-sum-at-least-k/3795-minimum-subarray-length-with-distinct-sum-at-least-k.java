344bdfc630978d1c586affba247dafde334199a1
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-10 18:09:15 +0300

    Time: 155 ms (24.44%), Space: 200 MB (28.2%) - LeetHub

---
+++ 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minLength(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int l = 0, sum = 0, ans = Integer.MAX_VALUE;
+        for (int r = 0; r < nums.length; r++) {
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+            if (map.get(nums[r]) == 1) {
+                sum += nums[r];
+            }
+            while (sum >= k) {
+                ans = Math.min(ans, r - l + 1);
+                map.put(nums[l], map.get(nums[l]) - 1);
+                if (map.get(nums[l]) == 0) {
+                    sum -= nums[l];
+                    map.remove(nums[l]);
+
+                }
+
+                l++;
+            }
+        }
+        if (ans == Integer.MAX_VALUE)
+            return -1;
+        return ans;
+    }
+}
