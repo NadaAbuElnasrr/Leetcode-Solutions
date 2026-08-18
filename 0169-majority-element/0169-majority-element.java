72a20ed067d64939a866d43e7fdd1af351dabdda
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-18 08:10:57 +0300

    Time: 2 ms (70.96%), Space: 55.6 MB (47.08%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int el=nums[0],cnt=1;
+        for(int i=1;i<nums.length;i++){
+            if(cnt==0){
+                el= nums[i];
+                cnt++;
+            }
+            else if(nums[i]==el) cnt++;
+            else cnt--;
+        }
+        return el;
+    }
+}
