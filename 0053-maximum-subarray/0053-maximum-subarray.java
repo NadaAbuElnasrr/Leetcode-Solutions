3b5eb65411316bca1e2a4c833e147cf74f26099c
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-18 17:04:52 +0300

    Time: 1 ms (99.95%), Space: 77.2 MB (49.32%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,10 +1,11 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max=Integer.MIN_VALUE,sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-            max=Math.max(sum,max);
-            sum=Math.max(sum,0);
+        //Kadane's Algorithm
+        int max = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(sum, max);
+            sum = Math.max(sum, 0);
         }
         return max;
     }
