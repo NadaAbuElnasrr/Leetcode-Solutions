5fe9814bbab4811fafd15f9fbd5ad8b2efacef6f
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-18 08:14:19 +0300

    Time: 1 ms (99.89%), Space: 55.4 MB (69.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,13 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int el=nums[0],cnt=1;
-        for(int i=1;i<nums.length;i++){
-            if(cnt==0){
-                el= nums[i];
+        //moore's voting algo
+        int el = nums[0], cnt = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (cnt == 0) {
+                el = nums[i];
                 cnt++;
-            }
-            else if(nums[i]==el) cnt++;
-            else cnt--;
+            } else if (nums[i] == el)
+                cnt++;
+            else
+                cnt--;
         }
         return el;
     }
