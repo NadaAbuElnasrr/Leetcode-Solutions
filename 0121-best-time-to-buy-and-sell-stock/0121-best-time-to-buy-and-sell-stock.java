53f2d2717be6ab34cb95f4437f22379210f76417
Nada Abu Elnasr <nada.abu.elnasr2@gmail.com>
2026-08-18 19:34:00 +0300

    Time: 1 ms (99.95%), Space: 94.7 MB (15.79%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //Dinamic
-        int l = 0, ans = 0;
-        for (int r = 0; r < prices.length; r++) {
-            while (prices[r] < prices[l]) {
-                l++;
-            }
-            ans = Math.max(ans, prices[r] - prices[l]);
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            profit = Math.max(profit, prices[i] - min);
+            min = Math.min(min, prices[i]);
         }
-        return ans;
+        return profit;
     }
 }
